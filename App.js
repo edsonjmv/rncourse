633f4663e0691e5cdaac0899dfcee39bb4380647
MACHE022 <edson.machado.-nd@disney.com>
2022-10-30 16:59:04 +0100

    Styling react-native apps

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-      <Text>Another text</Text>
+      <Text style={styles.dummyText}>Hello World!!!</Text>
+      <Text style={styles.dummyText}>Another text</Text>
       <Button title="Tap me!" />
     </View>
   );
@@ -16,5 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue'
   }
 });
