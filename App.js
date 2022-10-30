9b4ec24b39922ceaf989c69b13e147349aa5abad
MACHE022 <edson.machado.-nd@disney.com>
2022-10-30 18:26:05 +0100

    Styling goal item

--- App.js
+++ App.js
@@ -26,7 +26,9 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
         {goals.map((goal) => (
-          <Text key={goal}>{goal}</Text>
+          <View key={goal} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
         ))}
       </View>
     </View>
@@ -57,5 +59,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc'
+  },
+  goalText: {
+    color: 'white'
   }
 });
